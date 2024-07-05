3c2b33516b8af4ec9411fb4c8019de1dfafd2411
suchith kumar <suchithkumargm@gmail.com>
2024-07-05 16:28:09 +0530

    implemented transactions

--- server/controllers/auth/userAuthController.js
+++ server/controllers/auth/userAuthController.js
@@ -18,15 +18,22 @@ export const registerUser = async (req, res) => {
         return res.status(400).json({ errors: errors.array() });
     }
 
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
     try {
         // Check if a user with the same userName and email already exists
         let existingUserEmail = await User.findOne({ email: req.body.email });
         let existingUserName = await User.findOne({ userName: req.body.userName });
 
         if (existingUserEmail) {
+            await session.abortTransaction();
+            session.endSession();
             return res.status(400).json({ error: "Sorry, a user with this email already exists!" });
         }
         if (existingUserName) {
+            await session.abortTransaction();
+            session.endSession();
             return res.status(400).json({ error: "Sorry, a user with this UserName already exists!" });
         }
 
@@ -35,7 +42,7 @@ export const registerUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
         // Create a new user record in the database
-        const user = await User.create({
+        const newUser = new User({
             name: req.body.name,
             email: req.body.email,
             userName: req.body.userName,
@@ -47,16 +54,26 @@ export const registerUser = async (req, res) => {
             cash: req.body.cash,
         });
 
+        await newUser.save({ session });
+
         // Create an authentication token (JWT) for the new user
         const data = {
             user: {
-                userName: user.userName,
+                userName: newUser.userName,
             },
         };
         const authToken = jwt.sign(data, JWT_SECRET);
 
         res.json({ authToken });
+
+        // Commit the transaction
+        await session.commitTransaction();
+        session.endSession();
     } catch (error) {
+        // Rollback the transaction on error
+        await session.abortTransaction();
+        session.endSession();
+
         console.error(error);
         res.status(500).send("Internal Server Error");
     }
